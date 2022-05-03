e856a2686d37db5918073b3f050e7c6e96bbbc0e
Wagner Ribas <wagnerribas63@gmail.com>
2022-05-02 23:40:06 -0300

    [ REPOSITORIES ] EloquentAccountRepo implementation

---
+++ app/Repositories/Accounts/EloquentAccountsRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories\Accounts;
+
+use App\Repositories\Accounts\IAccountsRepository;
+use App\Models\Account;
+
+class EloquentAccountsRepository implements IAccountsRepository {
+
+    private Account $accountModel;
+
+    public function __construct()
+    {
+        $this->accountModel = new Account;
+    }
+
+    public function findById(int|string $id): array
+    {
+        return $this->accountModel->where('id', $id)->get();
+    }
+
+    public function findByNumber(int|string $number): array
+    {
+        return $this->accountModel->where('number', $number)->get();
+    }
+
+    public function create(array $data): void {
+        $this->accountModel->create($data);
+    }
+
+    public function getBalanceByNumber(int|string $number): int
+    {
+        return $this->accountModel->where('number', $number)->get('balance');
+    }
+}
