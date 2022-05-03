5b9a68e56776ecacc240fb99b6a555c658d251b2
Wagner Ribas <wagnerribas63@gmail.com>
2022-05-02 23:42:50 -0300

    [ REPOSITORIES ] EloquentEventsRepo implementation

---
+++ app/Repositories/Events/EloquentEventsRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories\Accounts;
+
+use App\Repositories\Events\IEventsRepository;
+use App\Models\Event;
+
+class EloquentAccountsRepository implements IEventsRepository {
+
+    private Event $eventModel;
+
+    public function __construct()
+    {
+        $this->eventModel = new Event;
+    }
+
+    public function findById(int|string $id): array
+    {
+        return $this->eventModel->where('id', $id)->get();
+    }
+
+    public function create(array $data): void {
+        $this->eventModel->create($data);
+    }
+
+  
+}
